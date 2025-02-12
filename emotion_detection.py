ee1f2c64709333ca2ac74ad7cae5cf75cfa86c9e
ianes-gm1 <prealaianes@gmail.com>
2025-02-12 13:37:40 -0500

    task-2

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,5 +7,5 @@ def emotion_detector(text_to_analyse):
     response = requests.post(url, json = obj, headers=header)
     return response.text
 
-#from emotion_detection import emotion_detector
+#from emotion_detection import emotion_detecto
 #emotion_detector("I love this new technology.")

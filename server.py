60fa84320b42659fe9cbf3812751c2710401393a
Ianis <90624583+ianes-gm1@users.noreply.github.com>
2025-02-12 23:22:32 +0100

    task 8

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+"""
+    server.py -A simple web server made with Flask
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,16 +8,22 @@ app = Flask("Emotion Detector")
 
 @app.route("/")
 def render_index_page():
+    """
+    Renders the index.html template
+    """
     return render_template('index.html')
 
 @app.route("/emotionDetector")
 def sent_analyzer():
+    """
+    The text analyzing action!
+    """
     text_to_analyze = request.args.get('textToAnalyze')
 
-    response = emotion_detector(text_to_analyze)
-    dominant_emotion = response.pop("dominant_emotion", None)
+    res = emotion_detector(text_to_analyze)
+    dom_e = res.pop("dominant_emotion", None)
 
-    return "For the given statement, the system response is {} The dominant emotion is {}.".format(response, dominant_emotion)
+    return f"For the given statement, the system response is {res} The dominant emotion is {dom_e}."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
